8cf6f4675bd2165852736bb289f14d0cc8ccfa1a
Alexander R. Hankin <alexander.hankin@tufts.edu>
2018-12-02 16:46:42 -0500

    bug fix in out of bounds test

--- cpp_test_wrappers/test_outofbounds.cpp
+++ cpp_test_wrappers/test_outofbounds.cpp
@@ -26,10 +26,9 @@ void operator delete(void* block)
 
 int main(int argc, char* argv[])
 {
-    Array<int> a(5, 0);
-    int result = 0;
+    Array a(5, 0);
     try {
-          result = a[10];
+          std::cout << a[10] << std::endl;
           std::cout << "Index is out of bounds -- should throw exception" << std::endl;
     }
     catch (const std::exception& e){
