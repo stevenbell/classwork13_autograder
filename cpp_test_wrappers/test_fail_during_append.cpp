7069656e52ee211c0f6a5a69728a56cffce263d7
Alexander R. Hankin <alexander.hankin@tufts.edu>
2018-12-02 23:04:47 -0500

    fix new-fail-during-append

--- cpp_test_wrappers/test_fail_during_append.cpp
+++ cpp_test_wrappers/test_fail_during_append.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <iostream>
 #include "array.h"
+
 // Global flag that sets whether or not `new` will fail
 bool memoryAllocFail = false; // Succeed by default
 
@@ -27,9 +28,19 @@ void operator delete(void* block)
 int main(int argc, char* argv[])
 {
     memoryAllocFail = true;
-    Array<int> a 
+    Array<int> a(5, 0);
+    a.append(1);
+     
+    if(a.size() != 5){
+        printf("###Expected size of %d but got %d###", 5, a.size());
+        return(0);
+    }
 
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != 0){
+            printf("###Couldn't access value %d###", i);
+        }
+    }   
 
-    return(0);
 }
 
